c1f3e07b472137c34bb57ac5aab7fcaafb7cbda5
Annika Rodriguez <alrodriguez3@usfca.edu>
2019-02-15 18:19:37 -0800

    commit files

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,16 +2,24 @@ public class BubbleSort implements SortingAlgorithm {
 
 
     public void sort(int[] a){
+        //iterates through entire array and swaps if current is greater than the next one
+        //sorts in ascending order
 
         for (int i= 0; i < a.length - 1; i++){
             for (int j= 0; j < a.length - i - 1; j++){
                 if (a[j] > a[j+1]){
-                    int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+                    swap(a, j, j+1);
                 }
             }
         }
+
+    }
+
+    public void swap(int[] array, int a, int b){
+
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
 
     }
 
